6905b50b82f6d19f9dded28ba2f495f1442f2cbd
Reazul Iqbal <admin@reazul.net>
2021-09-01 17:14:38 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { version } from './package.json'
 
 export const TOKEN = 'PAL'
 export const IS_HIVE = true
-export const APP_DOMAIN = 'https://palnet.io'
+export const APP_DOMAIN = 'https://www.palnet.io'
 export const APP_TITLE = 'Palnet'
 export const APP = `outpost/${version}`
 export const IMAGES_CDN = 'https://images.hive.blog/'
