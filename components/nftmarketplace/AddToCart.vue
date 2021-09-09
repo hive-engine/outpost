69699d0625481d10d0293d918bf3b5090c4db9cc
Reazul Iqbal <admin@reazul.net>
2021-09-09 09:12:43 +0600

    Multiple NFT selection with Shift+Click

--- components/nftmarketplace/AddToCart.vue
+++ components/nftmarketplace/AddToCart.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="add-to-cart">
     <template v-if="cartItemExist">
-      <b-button variant="danger" size="sm" @click="removeFromCart(nft)">
+      <b-button variant="danger" size="sm" @click="removeFromCart(nft, $event)">
         <fa-icon icon="cart-arrow-down" />
       </b-button>
     </template>
 
     <template v-else>
-      <b-button variant="primary" size="sm" @click="addToCart(nft)">
+      <b-button variant="primary" size="sm" @click="addToCart(nft, $event)">
         <fa-icon icon="cart-plus" />
       </b-button>
     </template>
@@ -21,7 +21,8 @@ export default {
   name: 'AddToCart',
 
   props: {
-    nft: { type: Object, required: true }
+    nft: { type: Object, required: true },
+    items: { type: Array, required: true }
   },
 
   computed: {
@@ -33,14 +34,48 @@ export default {
   },
 
   methods: {
-    ...mapMutations('nftmarketplace', ['ADD_TO_CART', 'REMOVE_FROM_CART']),
+    ...mapMutations('nftmarketplace', ['ADD_TO_CART', 'REMOVE_FROM_CART', 'EMPTY_CART']),
 
-    addToCart (nft) {
-      this.ADD_TO_CART(nft)
+    addToCart (nft, event) {
+      if (event.shiftKey) {
+        const items = JSON.parse(JSON.stringify(this.items))
+
+        const lastAdded = this.cart.filter(c => c.series === this.nft.series)
+
+        if (lastAdded.length <= 0) { this.ADD_TO_CART(nft) }
+
+        const lastItem = lastAdded[lastAdded.length - 1] || nft
+        const lastIndex = items.findIndex(c => c.nft_id === lastItem.nft_id)
+        const currentIndex = items.findIndex(c => c.nft_id === nft.nft_id)
+
+        const start = (lastIndex > currentIndex) ? currentIndex : lastIndex + 1
+        const end = (lastIndex > currentIndex) ? lastIndex : currentIndex + 1
+
+        items.slice(start, end).forEach(c => this.ADD_TO_CART(c))
+      } else {
+        this.ADD_TO_CART(nft)
+      }
     },
 
-    removeFromCart ({ nft_id: nftId }) {
-      this.REMOVE_FROM_CART(nftId)
+    removeFromCart ({ nft_id: nftId }, event) {
+      if (event.shiftKey) {
+        const items = JSON.parse(JSON.stringify(this.items))
+
+        const lastAdded = this.cart.filter(c => c.series === this.nft.series)
+
+        if (lastAdded.length > 0) { this.REMOVE_FROM_CART(lastAdded[lastAdded.length - 1].nft_id) }
+
+        const lastItem = lastAdded[lastAdded.length - 1] || { nft_id: nftId }
+        const lastIndex = items.findIndex(c => c.nft_id === lastItem.nft_id)
+        const currentIndex = items.findIndex(c => c.nft_id === nftId)
+
+        const start = (lastIndex > currentIndex) ? currentIndex : lastIndex + 1
+        const end = (lastIndex > currentIndex) ? lastIndex : currentIndex + 1
+
+        this.EMPTY_CART(items.slice(start, end).map(n => n.nft_id))
+      } else {
+        this.REMOVE_FROM_CART(nftId)
+      }
     }
   }
 }
