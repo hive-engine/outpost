97dd41a907edaec10b294160cabcd77e882e9ef7
Reazul Iqbal <admin@reazul.net>
2021-09-16 02:28:02 +0000

    Update config.js

--- config.js
+++ config.js
@@ -20,16 +20,16 @@ export const CURATED_FEED_ACCOUNT = 'r-cine'
 export const SIDECHAIN_ID = 'ssc-mainnet-hive'
 export const SIDECHAIN_RPC = 'https://ha.herpc.dtools.dev'
 export const SIDECHAIN_EXPLORER = 'https://he.dtools.dev'
-export const SIDECHAIN_HISTORY_API = 'https://accounts.hive-engine.com'
+export const SIDECHAIN_HISTORY_API = 'https://history.hive-engine.com'
 export const NUMBER_OF_UNSTAKE_TRX = 6
 export const MAX_UPLOAD_SIZE = 8
 export const AXIOS_CACHE_CONFIG = {
   maxAge: 0 * 60 * 1000, // 0 minutes
   exclude: { query: false }
 }
-export const NFT_ENABLED = false
-export const NFT_SYMBOL = 'CINENFT'
-export const NFT_MARKETPLACE = 'cinetv'
+export const NFT_ENABLED = true
+export const NFT_SYMBOL = 'LISTNFT'
+export const NFT_MARKETPLACE = 'hivelist'
 export const NFT_MARKETPLACE_API = 'https://api.marketplace.tribaldex.com'
 export const DTF_ENABLED = false
 export const DTF_ID = 'CINE:CINE'
