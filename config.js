3e878cb510e581785f3e44866e9b06a91816050a
hivelist <70282173+hivelist@users.noreply.github.com>
2021-10-01 23:25:08 -0700

    enabling LIST pool

--- config.js
+++ config.js
@@ -33,5 +33,5 @@ export const NFT_MARKETPLACE = 'hivelist'
 export const NFT_MARKETPLACE_API = 'https://api.marketplace.tribaldex.com'
 export const DTF_ENABLED = false
 export const DTF_ID = 'CINE:CINE'
-export const POOL_ENABLED = false
+export const POOL_ENABLED = true
 export const IPFS_GATEWAY = 'https://ipfs.tribaldex.com'
