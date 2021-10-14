3a420b4d28576a3263a7f92ab962ef37296c8d15
Emil Mendoza <em@king.net>
2021-10-14 11:10:19 -0400

    updated config.js for sextoken

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 import { version } from './package.json'
 
-export const TOKEN = 'LASSECASH'
+export const TOKEN = 'SEXTOKEN'
 export const IS_HIVE = true
-export const APP_DOMAIN = 'https://www.lassecash.com'
-export const APP_TITLE = 'LasseCash'
-export const APP = `lassecash/${version}`
+export const APP_DOMAIN = 'https://www.sextoken.com'
+export const APP_TITLE = 'SexToken.com'
+export const APP = `sextoken/${version}`
 export const IMAGES_CDN = 'https://images.hive.blog/'
 export const IMAGE_UPLOAD_SERVER = 'https://images.hive.blog'
 export const NODES = ['https://api.hive.blog', 'https://api.deathwing.me', 'https://rpc.ausbit.dev', 'https://api.ha.deathwing.me']
-export const COMMUNITY_CATEGORY = 'hive-145209'
+export const COMMUNITY_CATEGORY = 'hive-105477'
 export const AUTO_ADD_COMMUNITY = false
-export const SCOT_TAG = 'lassecash'
+export const SCOT_TAG = 'sextoken'
 export const SCOT_TAG_FIRST = true
 export const MAX_TAG = 20
 export const SCOT_API = 'https://scot-api.hive-engine.com'
 export const SCOT_QUERY_LIMIT = 10
 export const CURATED_FEED = false
-export const CURATED_FEED_ACCOUNT = 'lasseehlers'
+export const CURATED_FEED_ACCOUNT = 'sextoken'
 export const SIDECHAIN_ID = 'ssc-mainnet-hive'
 export const SIDECHAIN_RPC = 'https://ha.herpc.dtools.dev'
 export const SIDECHAIN_EXPLORER = 'https://he.dtools.dev'
@@ -27,11 +27,11 @@ export const AXIOS_CACHE_CONFIG = {
   maxAge: 0 * 60 * 1000, // 0 minutes
   exclude: { query: false }
 }
-export const NFT_ENABLED = true
-export const NFT_SYMBOL = 'NFTLC'
-export const NFT_MARKETPLACE = 'lassecash'
+export const NFT_ENABLED = false
+export const NFT_SYMBOL = 'NFTSEX'
+export const NFT_MARKETPLACE = 'sextoken'
 export const NFT_MARKETPLACE_API = 'https://api.marketplace.tribaldex.com'
 export const DTF_ENABLED = false
 export const DTF_ID = ''
-export const POOL_ENABLED = true
+export const POOL_ENABLED = false
 export const IPFS_GATEWAY = 'https://ipfs.tribaldex.com'
