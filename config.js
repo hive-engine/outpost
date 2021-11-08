8eb089a888387d36d097d7f2bdea8fcd37427744
Reazul Iqbal <admin@reazul.net>
2021-11-08 03:15:51 +0000

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ export const AXIOS_CACHE_CONFIG = {
   maxAge: 0 * 60 * 1000, // 0 minutes
   exclude: { query: false }
 }
-export const NFT_ENABLED = false
+export const NFT_ENABLED = true
 export const NFT_SYMBOL = 'SPORTS'
 export const NFT_MARKETPLACE = 'sportstalksocial'
 export const NFT_MARKETPLACE_API = 'https://api.marketplace.tribaldex.com'
