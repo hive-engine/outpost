5274baca6a377615fbec18a3d6e192509d9e3cbd
Reazul Iqbal <admin@reazul.net>
2022-04-26 10:03:03 +0600

    Moving to SMT

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 import { version } from './package.json'
 
-export const TOKEN = 'PAL'
+export const TOKEN = 'NEOXAG'
 export const IS_HIVE = true
-export const APP_DOMAIN = 'https://www.palnet.io'
-export const APP_TITLE = 'PALnet'
-export const APP = `palnet/${version}`
+export const APP_DOMAIN = 'https://www.neoxian.city'
+export const APP_TITLE = 'Neoxian City'
+export const APP = `neoxian/${version}`
 export const IMAGES_CDN = 'https://images.hive.blog/'
 export const IMAGE_UPLOAD_SERVER = 'https://images.hive.blog'
 export const NODES = ['https://api.hive.blog', 'https://api.deathwing.me', 'https://rpc.ausbit.dev', 'https://api.ha.deathwing.me']
-export const COMMUNITY_CATEGORY = 'hive-110490'
+export const COMMUNITY_CATEGORY = 'hive-177682'
 export const AUTO_ADD_COMMUNITY = true
-export const SCOT_TAG = 'palnet'
-export const SCOT_TAG_FIRST = true
+export const SCOT_TAG = 'neoxian'
+export const SCOT_TAG_FIRST = false
 export const MAX_TAG = 10
 export const ADD_COMMEMT_FOOTER = true
 export const COMMENT_FOOTER = `Posted using [${APP_TITLE}](%post_url%)`
@@ -23,18 +23,18 @@ export const SIDECHAIN_ID = 'ssc-mainnet-hive'
 export const SIDECHAIN_RPC = 'https://ha.herpc.dtools.dev'
 export const SIDECHAIN_EXPLORER = 'https://he.dtools.dev'
 export const SIDECHAIN_HISTORY_API = 'https://history.hive-engine.com'
-export const NUMBER_OF_UNSTAKE_TRX = 4
+export const NUMBER_OF_UNSTAKE_TRX = 13
 export const MAX_UPLOAD_SIZE = 8
 export const AXIOS_CACHE_CONFIG = {
   maxAge: 0 * 60 * 1000, // 0 minutes
   exclude: { query: false }
 }
-export const NFT_ENABLED = true
-export const NFT_SYMBOL = 'PAL'
-export const NFT_MARKETPLACE = 'palnet'
+export const NFT_ENABLED = false
+export const NFT_SYMBOL = 'NEOXAG'
+export const NFT_MARKETPLACE = 'neoxag'
 export const NFT_MARKETPLACE_API = 'https://api.marketplace.tribaldex.com'
-export const DTF_ENABLED = true
-export const DTF_ID = 'PAL:PAL'
+export const DTF_ENABLED = false
+export const DTF_ID = 'NEOXAG:NEOXAG'
 export const POOL_ENABLED = true
 export const IPFS_GATEWAY = 'https://ipfs.tribaldex.com'
 export const OUTPOST_ONBOARD = false
