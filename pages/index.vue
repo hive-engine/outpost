1fbcc09f8ca3f54b0b0883afdfc4513dd1950768
ZeroOoC <nikki@nyx-labs.com>
2022-07-18 17:39:17 +0200

    Content Change Test
    Changing a word to see if it takes.

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
             <div class="fade-in-text">
               <h1>What is Cine TV?</h1>
             </div>
-            <p>A supportive Hive-based platform for people to explore their passion for cinema, tv and theater through the creative process of blogging and earn crypto while doing so.</p>
+            <p>It's a supportive Hive-based platform for people to explore their passion for cinema, tv and theater through the creative process of blogging and earn crypto while doing so.</p>
             <h4>Current CINE Price: {{ getUSDPrice(1) }}</h4>
             <div class="d-flex justify-content-between align-items-center">
               <svg
