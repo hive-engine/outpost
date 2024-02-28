ca65c91d6ecfa66f114708c65226a310203e0d68
hivelist <70282173+hivelist@users.noreply.github.com>
2024-02-28 12:26:39 -0500

    Update SidebarMenu.vue
    Changing some menu items on the sidebar

--- components/SidebarMenu.vue
+++ components/SidebarMenu.vue
@@ -12,19 +12,15 @@
       <b-list-group-item class="font-weight-bold" disabled>
         Menu
       </b-list-group-item>
-
-      <b-list-group-item v-if="$auth.loggedIn" :to="{name:'user-feed', params:{user: $auth.user.username}}">
-        Feed
+      <b-list-group-item target="_blank" href="https://hivelist.io">
+        Home <fa-icon icon="external-link-alt" />
       </b-list-group-item>
-
-      <b-list-group-item :to="{name:'sort', params:{sort:'trending'}}">
-        Explore
+      <b-list-group-item target="_blank" href="https://hivelist.io/tokenomics">
+        Tokenomics <fa-icon icon="external-link-alt" />
       </b-list-group-item>
-
-      <b-list-group-item v-if="$config.CURATED_FEED && $config.CURATED_FEED_ACCOUNT !== ''" :to="{name:'sort', params:{sort:'curated'}}">
-        Curator's Pick
+      <b-list-group-item target="_blank" href="https://hivelist.store">
+        Store <fa-icon icon="external-link-alt" />
       </b-list-group-item>
-
       <b-list-group-item v-if="$config.NFT_ENABLED" :to="{name:'nfts'}">
         NFTs
       </b-list-group-item>
@@ -34,11 +30,20 @@
       </b-list-group-item>
 
       <b-list-group-item v-if="$config.POOL_ENABLED" :to="{name:'pool'}">
-        Swap Pool
+        Pool
       </b-list-group-item>
       
        <b-list-group-item class="font-weight-bold" disabled>
-       Categories
+       Classifieds
+      </b-list-group-item>
+      <b-list-group-item v-if="$auth.loggedIn" :to="{name:'user-feed', params:{user: $auth.user.username}}">
+        Feed
+      </b-list-group-item>
+      <b-list-group-item :to="{name:'sort', params:{sort:'trending'}}">
+        Explore
+      </b-list-group-item>
+      <b-list-group-item v-if="$config.CURATED_FEED && $config.CURATED_FEED_ACCOUNT !== ''" :to="{name:'sort', params:{sort:'curated'}}">
+        Curator's Pick
       </b-list-group-item>
       <b-list-group-item :to="{name:'sort-tag', params:{ sort:'created', tag:'forsale'}}">
         For Sale
@@ -65,23 +70,8 @@
         Wanted
       </b-list-group-item>
     </b-list-group>
-
-    <b-list-group class="mt-3" flush>
-       <b-list-group-item class="font-weight-bold" disabled>
-       Hivelist.io
-      </b-list-group-item>
-      <b-list-group-item target="_blank" href="https://hivelist.io">
-        Home <fa-icon icon="external-link-alt" />
-      </b-list-group-item>
-
-      <b-list-group-item target="_blank" href="https://hivelist.io/tokenomics">
-        Tokenomics <fa-icon icon="external-link-alt" />
-      </b-list-group-item>
-
-      <b-list-group-item target="_blank" href="https://hivelist.store">
-        Hivelist Store <fa-icon icon="external-link-alt" />
-      </b-list-group-item>
-
+      
+    <b-list-group class="d-lg-none" flush>
       <b-list-group-item class="font-weight-bold" disabled>
         Trade
       </b-list-group-item>
