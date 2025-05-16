e73a90fad737f5aec8bb50c05737018fa0cec6aa
Bjorn Beheydt <bjorn@beheydt.be>
2025-05-16 13:27:31 +0200

    Update Header.vue
    2nd Try at login-fix

--- components/Header.vue
+++ components/Header.vue
@@ -99,7 +99,7 @@
             </b-nav-item-dropdown>
           </template>
 
-<template v-if!="$auth.loggedIn">
+<template v-else>
 
     <b-nav-item @click.prevent="$bvModal.show('loginModal')">
       Login
